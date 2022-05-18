73dcdafdeb3c0c17b7aab694228703704d281627
Diego <luizdiego.silvabarbosa@gmail.com>
2022-05-18 18:13:45 -0300

    Adicionado ';'

--- JS/Aula 10/script.js
+++ JS/Aula 10/script.js
@@ -1,4 +1,4 @@
-alert( "Checar console!!!" )
+alert( "Checar console!!!" );
 
 class Person
 {
@@ -11,10 +11,10 @@ class Person
     get age ()
     {
         let age = currentDate.getFullYear() - this.birthday.getFullYear();
-        let currentDay = currentDate.getDate()
-        let currentMonth = currentDate.getMonth()
-        let birthDay = this.birthday.getDate()
-        let birthMonth = this.birthday.getMonth()
+        let currentDay = currentDate.getDate();
+        let currentMonth = currentDate.getMonth();
+        let birthDay = this.birthday.getDate();
+        let birthMonth = this.birthday.getMonth();
 
         if ( currentMonth < birthMonth || ( currentMonth == birthMonth && currentDay < birthDay ) )
         {
@@ -26,10 +26,10 @@ class Person
 
     get hasCompleteBirthday ()
     {
-        let currentDay = currentDate.getDate()
-        let currentMonth = currentDate.getMonth()
-        let birthDay = this.birthday.getDate()
-        let birthMonth = this.birthday.getMonth()
+        let currentDay = currentDate.getDate();
+        let currentMonth = currentDate.getMonth();
+        let birthDay = this.birthday.getDate();
+        let birthMonth = this.birthday.getMonth();
 
         if ( currentMonth < birthMonth || ( currentMonth == birthMonth && currentDay < birthDay ) )
         {
@@ -40,22 +40,22 @@ class Person
     }
 }
 
-const currentDate = new Date()
+const currentDate = new Date();
 
 const persons = new Array( 10 );
-persons[0] = new Person( "Diego", new Date( 2003, 4, 31) )
-persons[1] = new Person( "Davi", new Date( 2001, 11, 18) )
-persons[2] = new Person( "Daniel", new Date( 2005, 3, 17) )
-persons[3] = new Person( "Diogo", new Date( 2003, 6, 25) )
-persons[4] = new Person( "Danilo", new Date( 2008, 7, 1) )
-persons[5] = new Person( "David", new Date( 1998, 9, 27) )
-persons[6] = new Person( "Douglas", new Date( 1997, 1, 29) )
-persons[7] = new Person( "Derik", new Date( 1999, 2, 13) )
-persons[8] = new Person( "Domingos", new Date( 2000, 21, 31) )
-persons[9] = new Person( "Denis", new Date( 2002, 8, 20) )
+persons[0] = new Person( "Diego", new Date( 2003, 4, 31) );
+persons[1] = new Person( "Davi", new Date( 2001, 11, 18) );
+persons[2] = new Person( "Daniel", new Date( 2005, 3, 17) );
+persons[3] = new Person( "Diogo", new Date( 2003, 6, 25) );
+persons[4] = new Person( "Danilo", new Date( 2008, 7, 1) );
+persons[5] = new Person( "David", new Date( 1998, 9, 27) );
+persons[6] = new Person( "Douglas", new Date( 1997, 1, 29) );
+persons[7] = new Person( "Derik", new Date( 1999, 2, 13) );
+persons[8] = new Person( "Domingos", new Date( 2000, 21, 31) );
+persons[9] = new Person( "Denis", new Date( 2002, 8, 20) );
 
 for ( let i = 0; i < persons.length; i ++ )
 {
-    let person = persons[i]
-    console.log( `Nome: ${person.name} Idade: ${person.age} Fez aniversário: ${person.hasCompleteBirthday}` )
+    let person = persons[i];
+    console.log( `Nome: ${person.name} Idade: ${person.age} Fez aniversário: ${person.hasCompleteBirthday}` );
 }
