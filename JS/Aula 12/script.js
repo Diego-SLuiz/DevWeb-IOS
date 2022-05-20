627f9ad36c0ed677a42e596b263fe37926eacc38
Diego <luizdiego.silvabarbosa@gmail.com>
2022-05-20 02:31:31 -0300

    Funcionalidades atividade JS Aula 12

--- JS/Aula 12/script.js
+++ JS/Aula 12/script.js
@@ -1,23 +1,113 @@
-function addStudent ()
+// Retornar um elemento "tr" com os dados inseridos pelo usuário
+function getStudentInput ()
 {
-    const studentForm = document.getElementById( "student-form" );
-    const studentContent = document.getElementById( "student-content" );
-    const userInput = studentForm.getElementsByTagName( "input" );
-    const studentRow = document.createElement( "tr" );
+    let inputFields = studentForm.getElementsByTagName( "input" )
+    let studentRow = document.createElement( "tr" );
 
-    for ( let i = 0; i < userInput.length; i ++ )
+    for ( let i = 0; i < inputFields.length; i ++ )
     {
         let studentData = document.createElement( "td" );
-        studentData.innerText = userInput[i].value
-
+        studentData.innerText = inputFields[i].value;
         studentRow.appendChild( studentData );
-        userInput[i].value = "";
-    } 
+    }
 
-    studentContent.appendChild( studentRow );
-    console.log( userInput )
+    return studentRow;
 }
 
-const studentConfirm = document.getElementById( "student-confirm" );
-studentConfirm.addEventListener( "click", addStudent );
- 
+// Limpar dados inseridos pelo usuário
+function clearStudentInput ()
+{
+    let inputFields = studentForm.getElementsByTagName( "input" );
+
+    for ( let i = 0; i < inputFields.length; i ++ )
+    {
+        inputFields[i].value = "";
+    }
+}
+
+// Buscar por um estudante já existente
+function searchStudentName ( studentName )
+{
+    let currentStudents = studentTbody.getElementsByTagName( "tr" );
+
+    for ( let i = 0; i < currentStudents.length; i ++ )
+    {
+        let currentName = currentStudents[i].children[0].innerText;
+
+        if ( studentName == currentName )
+        {
+            return currentStudents[i];
+        }
+    }
+}
+
+// Adicionar um novo estudante
+function addStudent ()
+{
+    let studentRow = getStudentInput();
+    let studentName = studentRow.children[0].innerText;
+    let targetStudent = searchStudentName( studentName );
+
+    if ( studentName && ! targetStudent )
+    {
+        studentTbody.appendChild( studentRow );
+        clearStudentInput();
+    }
+}
+
+// Remover um estudante existente
+function removeStudent ()
+{
+    let studentRow = getStudentInput();
+    let studentName = studentRow.children[0].innerText;
+    let targetStudent = searchStudentName( studentName );
+
+    if ( targetStudent )
+    {
+        targetStudent.remove();
+        clearStudentInput();
+    }
+}
+
+// Atualizar um estudante existente
+function updateStudent ()
+{
+    let studentRow = getStudentInput();
+    let studentName = studentRow.children[0].innerText;
+    let targetStudent = searchStudentName( studentName );
+
+    if ( targetStudent )
+    {
+        studentTbody.insertBefore( studentRow, targetStudent );
+        targetStudent.remove();
+        clearStudentInput();
+    }
+}
+
+// Apagar todos os estudantes existentes
+function resetStudent ()
+{
+    let currentStudents = studentTbody.getElementsByTagName( "tr" );
+    let length = currentStudents.length
+
+    for ( let i = 0; i < length; i ++ )
+    {
+        currentStudents[0].remove();
+    }
+
+    clearStudentInput();
+}
+
+const studentForm = document.getElementById( "student-form" );
+const studentTbody = document.getElementById( "student-tbody" );
+
+const addStudentButton = studentForm.querySelector( "#add-student-button" );
+const removeStudentButton = studentForm.querySelector( "#remove-student-button" );
+const updateStudentButton = studentForm.querySelector( "#update-student-button" );
+const resetStudentButton = studentForm.querySelector( "#reset-student-button" );
+
+addStudentButton.addEventListener( "click", addStudent );
+removeStudentButton.addEventListener( "click", removeStudent );
+updateStudentButton.addEventListener( "click", updateStudent );
+resetStudentButton.addEventListener( "click", resetStudent );
+
