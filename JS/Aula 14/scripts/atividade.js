41ef18cd999eb628fea94527f1cd2fc8b9a14700
Diego <luizdiego.silvabarbosa@gmail.com>
2022-05-25 01:56:07 -0300

    Finalizada atividade JS Aula 14

--- JS/Aula 14/scripts/atividade.js
+++ JS/Aula 14/scripts/atividade.js
@@ -1,51 +1,62 @@
 function getFormData ()
 {
-    // Get form data
+    // Buscar dados do formulário
     let form = document.getElementById( "main-form" );
     let input = form.getElementsByTagName( "input" );
     let data = [];
 
     for ( let i = 0; i < input.length; i ++ )
     {
-        if ( i == 1 )
-        {
-            data.push( new Date( input[i].value ) );
-            continue;
-        }
-
         data.push( input[i].value );
     }
 
-    console.log( data );
+    // Validar idade do usuário
+    let age = validateAge( new Date( data[1] ) )
 
-    // Validate form data
-    if ( ! validateAge( data[1], 18 ) ) // Age
+    if ( age < 18 || isNaN( age ) )
     {
-        alert( "menor de 18" );
+        alert( "Somente maiores de 18 anos!" );
         return;
+    }
+
+    // Validate email do usuário
+    if ( ! validateEmail( data[2] ) )
+    {
+        alert( "Insira um e-mail válido!" );
+        return;
+    }
+
+    data[1] = String( age )
+    return data;
+}
+
+function cleanFormData ()
+{
+    // Buscar dados do formulário
+    let form = document.getElementById( "main-form" );
+    let input = form.getElementsByTagName( "input" );
+
+    for ( let i = 0; i < input.length; i ++ )
+    {
+        input[i].value = "";
     }
 }
 
-function validateAge ( birthDate, minimumAge )
+function validateAge ( birthDate )
 {
     let currentYear = currentDate.getFullYear();
     let currentMonth = currentDate.getMonth();
     let currentDay = currentDate.getDate();
     let birthYear = birthDate.getFullYear();
     let birthMonth = birthDate.getMonth();
-    let birthDay = birthDate.getDate();
+    let birthDay = birthDate.getDate() + 1;
 
-    // Get current age
-    let age = birthYear - currentYear;
+    // Calcular idade e aniversário
+    let age = currentYear - birthYear;
 
-    if ( birthMonth < currentMonth || ( birthMonth == currentMonth && birthDay < currentDay ) )
+    if ( currentMonth < birthMonth || ( currentMonth == birthMonth && currentDay < birthDay ) )
     {
         age -= 1;
-    }
-
-    if ( age < minimumAge )
-    {
-        return 0;
     }
 
     return age;
@@ -53,32 +64,73 @@ function validateAge ( birthDate, minimumAge )
 
 function validateEmail ( email )
 {
-
-}
-
-function updatePerson ()
-{
-
+    // Validação por expressão regular
+    let emailExp = RegExp( /.*@.*\..*/ );
+    return emailExp.test( email );
 }
 
 function updateId ()
 {
+    let personContent = document.getElementById( "table-content" );
+    let personList = personContent.getElementsByTagName( "tr" );
 
+    // Atualizar ID em ordem crescente
+    for ( let i = 0; i < personList.length; i ++ )
+    {
+        personList[i].children[3].innerText = i + 1;
+    }
 }
 
 function addPerson ()
 {
+    let personContent = document.getElementById( "table-content" );
+    let person = document.createElement( "tr" );
+    let data = getFormData();
 
+    if ( ! data )
+    {
+        return
+    }
+
+    // Adicionar uma pessoa em tbody
+    for ( let i = 0; i < data.length; i ++ )
+    {
+        let personInfo = document.createElement( "td" );
+        personInfo.innerText = data[i];
+        person.appendChild( personInfo );
+    }
+
+    personContent.appendChild( person );
+    updateId();
+    cleanFormData();
 }
 
 function delPerson ()
 {
+    let personContent = document.getElementById( "table-content" );
+    let personList = personContent.getElementsByTagName( "tr" );
+    let targetId = document.getElementById( "item-id" ).value;
 
+    // Remover pessoa com ID desejado
+    for ( let i = 0; i < personList.length; i ++ )
+    {
+        let person = personList[i];
+        let personId = person.children[3].innerText;
+
+        if ( personId == targetId )
+        {
+            person.remove();
+            break
+        }
+    }
+
+    updateId()
+    cleanFormData()
 }
 
 const currentDate = new Date()
 const buttonAdd = document.getElementById( "button-add" );
 const buttonDel = document.getElementById( "button-del" );
 
-buttonAdd.addEventListener( "click", getFormData );
+buttonAdd.addEventListener( "click", addPerson );
 buttonDel.addEventListener( "click", delPerson );
