7f5569f1f1113b3d9d41d4915b13ebedb930932f
Diego <luizdiego.silvabarbosa@gmail.com>
2022-06-07 02:42:50 -0300

    Finalizada atividade REACT Aula 04

--- REACT/Aula 04/my-app/src/App.js
+++ REACT/Aula 04/my-app/src/App.js
@@ -1,14 +1,31 @@
-import Exemplo from './components/Exemplo';
-import Header from './components/Header';
+import Nota from './components/Nota';
+import Nome from './components/Nome';
 import './App.css';
 
 function App ()
 {
 	return (
-	<div>
-		<Exemplo/>
-		<Header nome="Diego"/>
-	</div>
+		<div className="main-app">
+			<div>
+				<Nome aluno="Diego"/>
+				<Nota notas={ [ 7, 8, 4, 6 ] }/>
+			</div>
+
+			<div>
+				<Nome aluno="João"/>
+				<Nota notas={ [ 10, 8, 9, 7 ] }/>
+			</div>
+
+			<div>
+				<Nome aluno="Maria"/>
+				<Nota notas={ [ 1, 2, 10, 6 ] }/>
+			</div>
+
+			<div>
+				<Nome/>
+				<Nota/>
+			</div>
+		</div>
 	)
 }
 
