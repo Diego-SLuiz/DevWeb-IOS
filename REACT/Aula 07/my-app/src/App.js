a02a7fe5c526b6bca5d28f5a8c6af64e52f75b7f
Diego <luizdiego.silvabarbosa@gmail.com>
2022-06-09 02:35:11 -0300

    Criando TaskItem e começando estilização

--- REACT/Aula 07/my-app/src/App.js
+++ REACT/Aula 07/my-app/src/App.js
@@ -7,21 +7,35 @@ const tempTasks =
 [
 	{
 		id: 1,
-		title: "Consulta no Dentista",
+		name: "Consulta no Dentista",
 		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio itaque repellendus dolorem nihil facere corrupti aliquid officiis veniam quibusdam quod.",
 		date: new Date(),
 		status: "pending",
 	},
 	{
 		id: 2,
-		title: "Reunião de Trabalho",
+		name: "Reunião de Trabalho",
 		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio itaque repellendus dolorem nihil facere corrupti aliquid officiis veniam quibusdam quod.",
 		date: new Date(),
 		status: "complete",
 	},
 	{
 		id: 3,
-		title: "Limpar a Casa",
+		name: "Limpar a Casa",
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio itaque repellendus dolorem nihil facere corrupti aliquid officiis veniam quibusdam quod.",
+		date: new Date(),
+		status: "incomplete",
+	},
+	{
+		id: 3,
+		name: "Limpar a Casa",
+		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio itaque repellendus dolorem nihil facere corrupti aliquid officiis veniam quibusdam quod.",
+		date: new Date(),
+		status: "incomplete",
+	},
+	{
+		id: 3,
+		name: "Limpar a Casa",
 		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio itaque repellendus dolorem nihil facere corrupti aliquid officiis veniam quibusdam quod.",
 		date: new Date(),
 		status: "incomplete",
@@ -30,14 +44,19 @@ const tempTasks =
 
 function App ()
 {
-	const [ tasks, setTasks ] = useState( tempTasks );
+	const [ pendingTasks, setPendingTasks ] = useState( tempTasks.filter( ( task ) => task.status == "pending" ) );
+	const [ completeTasks, setCompleteTasks ] = useState( tempTasks.filter( ( task ) => task.status == "complete" ) );
+	const [ incompleteTasks, setIncompleteTasks ] = useState( tempTasks.filter( ( task ) => task.status == "incomplete" ) );
+
+	let defaultMessage = ( pendingTasks.length | completeTasks.length | incompleteTasks.length ) ? null : <h2>Tudo quieto por aqui...</h2>;
 
 	return (
 		<div>
 			<Header title="Lista de Tarefas"/>
-			<TaskList title="Tarefas Concluídas" tasks={ tasks }/>
-			<TaskList title="Tarefas Pendentes" tasks={ tasks }/>
-			<TaskList title="Tarefas Passadas" tasks={ tasks }/>
+			{ pendingTasks.length ? <TaskList title="Tarefas Pendentes" tasks={ pendingTasks }/> : null }
+			{ completeTasks.length ? <TaskList title="Tarefas Concluídas" tasks={ completeTasks }/> : null }
+			{ incompleteTasks.length ? <TaskList title="Tarefas Antigas" tasks={ incompleteTasks }/> : null }
+			{ defaultMessage }
 		</div>
 	)
 }
--- REACT/Aula 07/my-app/src/components/TaskList.jsx
+++ REACT/Aula 07/my-app/src/components/TaskList.jsx
@@ -3,10 +3,10 @@ import TaskItem from "./TaskItem";
 function TaskList ( { title, tasks } )
 {
     return (
-        <div>
-            <h2>{ title }</h2>
+        <div className="task-list">
+            <h2 className="task-title" >{ title }</h2>
             <ul>
-                { tasks.map( ( task ) => <TaskItem/> ) }
+                { tasks.map( ( task ) => <TaskItem key={ task.id } task={ task }/> ) }
             </ul>
         </div>
     )
