3500cab83fc14ac38f8e928514d84c4479bb6c5f
Diego <luizdiego.silvabarbosa@gmail.com>
2022-06-16 01:31:38 -0300

    Finalizada atividade JAVA Aula 02

--- JAVA/Aula 02/Atividade/src/Main.java
+++ JAVA/Aula 02/Atividade/src/Main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-    public static void main ( String[] args )
+    public static void main ( String [] args )
     {
         // Aluno 1
         Aluno aluno_1 = new Aluno( "Diego" );
